a982bb1c283b4adacf6629efca618fba38e9351d
HAOYU-LI <lihaoyu001@gmail.com>
2017-11-12 20:04:36 +0800

    update row-wise augmentation

--- R/R_Wrapper.R
+++ R/R_Wrapper.R
@@ -8,7 +8,7 @@ UDC <- function(n,s,q,init="rand",initX=matrix(0),crit="MD2",maxiter=100000,hits
     q = max(initX) - min(initX) + 1
   }
   #restrictions for arguments:
-  if(is.double(n)&&is.double(s)&&is.double(q) == FALSE){stop("Wrong types of n,s,q.")}
+  if(is.numeric(n)&&is.numeric(s)&&is.numeric(q) == FALSE){stop("Wrong types of n,s,q.")}
   else if(n%%q != 0){stop("n should be multiple of q. ")}
   else if(s<=1 || n<=2 ){stop(("\n The size of design should be larger than 2*2."))}
   else if((init == "orth" || init == "input") && length(initX)==1){stop(("\n when init=Orth or input, input of initX is needed."))}
@@ -44,7 +44,7 @@ AUDC <- function (X0,n,s,q,init="rand",initX=matrix(0),crit="MD2",maxiter=100000
   #restrictions for arguments:
   if(n<=1 || s<=0 || q <=1 ){stop("Please input valid X0.")}
   else if(is.matrix(X0)==FALSE){stop("Please input X0 to do the augmented searching. End of program...")}
-  else if(is.double(n)&&is.double(s)&&is.double(q) == FALSE){stop("Wrong types of n,s,q. End of program...")}
+  else if(is.numeric(n)&&is.numeric(s)&&is.numeric(q) == FALSE){stop("Wrong types of n,s,q. End of program...")}
   else if(n%%q != 0){stop("n should multiple of q. End of program...")}
   else if(ncol(X0) != s){stop("Inconsistency in X0 columns and s. End of program...")}
   else if((init == "orth" || init == "input") && length(X0)==1){stop(("\n when init=Orth, input of X0 is needed."))}
@@ -99,4 +99,68 @@ Eval<-function(X0 = matrix(0), crit="MD2")
   return(StoEval(X0,q,crit))
 }
 
+RUDC <- function (X0,n,s,q,init="rand",initX=matrix(0),crit="MD2",J=10,maxiter=2000,
+                  hits_ratio = c(0.3,0.2,0.15,0.1,0.05,0.01),vis=FALSE)
+{
+
+  s = ncol(X0)
+  q = max(X0) - min(X0) + 1
+
+  #restrictions for arguments:
+  if(is.numeric(n)&&is.numeric(s)&&is.numeric(q) == FALSE){stop("Wrong types of n,s,q. End of program...")}
+  else if(is.matrix(X0)==FALSE){stop("Please input X0 to do the augmented searching. End of program...")}
+  else if(n<=1 || s<=0 || q <=1 ){stop("Please input valid X0.")}
+  else if(n%%q != 0){stop("n should multiple of q. End of program...")}
+  else if(ncol(X0) != s){stop("Inconsistency in X0 columns and s. End of program...")}
+
+  if(crit == "CL2"){crit=2}
+  else if(crit == "maximin"){crit=1}
+  else{crit=4}
+
+  #recall Rcpp compiled StoAUDC function:
+  #list0 <- StoAUDC(X0,n,s,q,init,initX,crit,maxiter,hits_ratio[1])
+  I = length(hits_ratio)
+  start_time = proc.time()
+  list_new <- UDC(n,s,q,init,initX,crit,maxiter,hits_ratio[1])
+  D1 = list_new$UniDOE_Matrix
+  D_global = rbind(X0,list_new$UniDOE_Matrix)
+  Dis_glob = Eval(D_global,crit=crit)
+  Dis_list = c()
+  Dis_list_cur = c()
+  for(i in 1:I)
+  {
+    for(j in 1:J)
+    {
+      D_tmp_list = UDC(n,s,q,init="input",initX=as.matrix(D1),crit,maxiter,hits_ratio[i])
+      D_tmp_1 = D_tmp_list$UniDOE_Matrix
+      D_tmp_cur = rbind(X0,D_tmp_1)
+      Dis_cur = Eval(D_tmp_cur,crit=crit)
+      Dis_list_cur = c(Dis_list_cur,Dis_cur)
+      if( Dis_cur < Dis_glob)
+      {
+        Dis_glob = Dis_cur
+        D1 = D_tmp_1
+        D_global = D_tmp_cur
+      }
+    }
+    Dis_list = c(Dis_list,Dis_list_cur)
+    Dis_list_cur = c()
+  }
+  return_list = list()
+  return_list$Aug_Matrix = D1
+  return_list$Objective_Matrix = D_global
+  return_list$obj = Dis_glob
+  return_list$obj_list = Dis_list
+  return_list$time_consumed = (proc.time() - start_time)[3]
+
+  if(vis == TRUE){
+    plot(return_list$obj_list,type="l")
+    bst_score = return_list$obj
+    min_index = which.min(return_list$obj_list)[1]
+    abline(v = min_index,col=2)
+    abline(h =return_list$obj_list[min_index],col=4 )
+    title(main = c("best_score = ",bst_score))
+  }
+  return(return_list)
+}
 
