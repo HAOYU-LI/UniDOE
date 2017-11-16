7dedc4f0dbeb69ec50b2e5d9ae609a0e3b89b17c
HAOYU-LI <lihaoyu001@gmail.com>
2017-11-16 17:20:16 +0800

    update AUDC in R_Wrapper.R

--- R/R_Wrapper.R
+++ R/R_Wrapper.R
@@ -34,30 +34,24 @@ UDC <- function(n,s,q,init="rand",initX=matrix(0),crit="MD2",maxiter=100000,hits
 }
 
 
-AUDC <- function (X0,n,s,q,init="rand",initX=matrix(0),crit="MD2",maxiter=100000,hits_ratio = 0.1,vis=FALSE)
+AUDC <- function (X0,n,crit="MD2",maxiter=100000,hits_ratio = 0.1,vis=FALSE)
 {
-  if(init=="input"){
-    n = nrow(initX)
-    s = ncol(initX)
-    q = max(initX) - min(initX) + 1
-  }
-  #restrictions for arguments:
-  if(n<=1 || s<=0 || q <=1 ){stop("Please input valid X0.")}
-  else if(is.matrix(X0)==FALSE){stop("Please input X0 to do the augmented searching. End of program...")}
-  else if(is.numeric(n)&&is.numeric(s)&&is.numeric(q) == FALSE){stop("Wrong types of n,s,q. End of program...")}
-  else if(n%%q != 0){stop("n should multiple of q. End of program...")}
-  else if(ncol(X0) != s){stop("Inconsistency in X0 columns and s. End of program...")}
-  else if((init == "orth" || init == "input") && length(X0)==1){stop(("\n when init=Orth, input of X0 is needed."))}
-  else if((init == "orth" || init == "input") && (nrow(X0) != n || range(X0)[2] != q)){
-  stop(("\n The size of Orthogonal matrix mismatches with n,q ."))
-  }else if((init == "orth") && ncol(X0) == s){stop(("\n If s = ncol(X0), then no update is proceeded."))}
 
+  s = ncol(X0)
+  q = max(X0) - min(X0) + 1
+  #restrictions for arguments:
+  if(n<=1 || s<=0 || q <=1 ){stop("Error: Please input valid X0.")}
+  else if(is.matrix(X0)==FALSE){stop("Error: Please input X0 to do the augmented searching. End of program...")}
+  else if(is.numeric(n) == FALSE){stop("Error: Wrong types of n. End of program...")}
+  else if(n%%q != 0){stop("Error: n should multiple of level. End of program...")}
+
+ 
   if(crit == "CL2"){crit=2}
   else if(crit == "maximin"){crit=1}
   else{crit=4}
 
   #recall Rcpp compiled StoAUDC function:
-  list <- StoAUDC(X0,n,s,q,init,initX,crit,maxiter,hits_ratio)
+  list <- StoAUDC(X0,n,s,q,init="rand",initX=matrix(0),crit,maxiter,hits_ratio)
   if(vis == TRUE){
     plot(list$obj_list,type="l")
     bst_score = list$obj
@@ -146,12 +140,8 @@ RUDC <- function (X0,n,s,q,init="rand",initX=matrix(0),crit="MD2",J=10,maxiter=2
     Dis_list = c(Dis_list,Dis_list_cur)
     Dis_list_cur = c()
   }
-  return_list = list()
-  return_list$Aug_Matrix = D1
-  return_list$Objective_Matrix = D_global
-  return_list$obj = Dis_glob
-  return_list$obj_list = Dis_list
-  return_list$time_consumed = (proc.time() - start_time)[3]
+  return_list = list(D1,D_global,Dis_glob,Dis_list,(proc.time() - start_time)[3])
+  names(return_list) = c(Aug_Matrix,Objective_Matrix,obj,obj_list,time_consumed)
 
   if(vis == TRUE){
     plot(return_list$obj_list,type="l")
